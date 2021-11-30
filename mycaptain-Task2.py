7351ce84d60520beac59230a9046979d38b623d9
Raghvendra Pratap Singh <87266692+raghavSingh03@users.noreply.github.com>
2021-12-01 01:01:12 +0530

    Add files via upload

---
+++ mycaptain-Task2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x=input("Input the Filename: ")
+a=x.split(".")
+print("The extension of the file is:"+(a[-1]))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
